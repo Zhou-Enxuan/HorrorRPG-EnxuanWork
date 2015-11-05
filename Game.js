46707b67f78b89611476c9bc251d55dfc3ca2b41
Zhou-Enxuan <enzhou64@gmail.com>
2015-11-04 17:35:16 -0800

    try to make room (success)

--- Game.js
+++ Game.js
@@ -1,20 +1,42 @@
 var mainState = {
 	preload: function() {
-		game.load.image('map','assests/images/2cd3b4fd5266d01625fb3110962bd40735fa352b.png' );
         game.load.spritesheet('character', 'assests/images/Horror game character sprite sheet.png',32,48);
+        game.load.image('woodFloor','assests/images/Tile map material/WoodFloor 3.png' );
+        game.load.image('woodWall','assests/images/Tile map material/cover wall.png' );
+        game.load.image('bed','assests/images/Tile map material/histopal bed 2.png' );
+        game.load.image('bookShell','assests/images/Tile map material/book shell.png' );
+        
+        
 	},
     
     create: function() {
-    
-        this.map = game.add.tileSprite(0,0,game.world.width, game.world.height, 'map');
-        this.map.tileScale.set(0.4);
+ 
+
+        this.room = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
+            for (var i=0; i<this.room.length; i++) {
+                for (var j = 0; j<this.room[i].length; j++) {
+                    if (this.room[i][j] === 1) {
+                        game.add.sprite(i*32,j*32,'woodFloor');
+                    } else {
+                        game.add.sprite(i*32,j*32,'woodWall');
+                    }
+                
+                }
+            
+            }
         this.sprite = game.add.sprite(300,10,'character')
         this.sprite.animations.add('flap', [1,2,3,0],10);
-        this.sprite.animations.add('flap2', [0])
-        this.cursor = game.input.keyboard.createCursorKeys();
+        this.sprite.animations.add('flap2', [0]);
+        
     },
     update: function() {
-         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+        
+        //Get Input
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
              this.sprite.x -= 2;
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
@@ -30,7 +52,8 @@ var mainState = {
         } else {
             this.sprite.animations.play('flap2');
         }
-            
+          
+        //Display Floor
         
     }
 }
