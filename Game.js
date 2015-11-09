afb2a176320ddb0d63edf3f20a3f18249cd9d8ef
Enxuan Zhou <enzhou64@gmail.com>
2015-11-08 23:12:53 -0800

    finish sprit sheet 1

--- Game.js
+++ Game.js
@@ -1,10 +1,11 @@
 var mainState = {
 	preload: function() {
         game.load.spritesheet('character', 'assests/images/Horror game character sprite sheet.png',32,48);
-        game.load.image('woodFloor','assests/images/Tile map material/WoodFloor 3.png' );
+        game.load.image('woodFloor','assests/images/Tile map material/WoodFloor 2.png' );
         game.load.image('woodWall','assests/images/Tile map material/cover wall.png' );
         game.load.image('bed','assests/images/Tile map material/histopal bed 2.png' );
         game.load.image('bookShell','assests/images/Tile map material/book shell.png' );
+        game.load.image('wall','assests/images/Tile map material/white wall.png' )
         
         
 	},
@@ -27,18 +28,44 @@ var mainState = {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
             for (var i=0; i<this.room.length; i++) {
-                for (var j = 0; j<this.room[0].length; j++) {
+                for (var j = 0; j<this.room[i].length; j++) {
                     if (this.room[i][j] === 1) {
-                        game.add.sprite(i*32,j*32,'woodFloor');
+                        game.add.sprite(i*32+160,j*32+64,'woodFloor');
                     } else if (this.room[i][j] === 0){
-                        var a = game.add.sprite(i*32,j*32,'woodWall', null, this.walls);
+                        var a = game.add.sprite(i*32+160,j*32+64,'woodWall', null, this.walls);
                         a.body.immovable = true;
+                    } else {
+                        game.add.sprite(i*32+160,j*32+64,'wall')
                     }
                 
                 }
             
             }
-        this.sprite = game.add.sprite(300,10,'character')
+         this.roomMaterial = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+         
+         for (var i=0; i<this.roomMaterial.length; i++) {
+                for (var j = 0; j<this.roomMaterial[i].length; j++) {
+                    if (this.roomMaterial[i][j] === 1) {
+                        game.add.sprite(i*32+160,j*32+64,'bookShell');
+                    } else if (this.roomMaterial[i][j] === 2) {
+                        game.add.sprite(i*32+160,j*32+64,'bed')
+                    }
+                
+                }
+            
+            }
+        this.sprite = game.add.sprite(224,192,'character')
         this.sprite.collideWorldBounds = true;
         game.physics.enable(this.sprite);
         this.sprite.animations.add('flap', [1,2,3,0],10);
